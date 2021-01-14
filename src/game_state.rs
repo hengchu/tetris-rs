3a30c2e1ad85f97a24119a2848f080f2d0468171
hengchu <hengchu.zhang@gmail.com>
2021-01-13 20:27:18 -0500

    implemented tick

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,6 +1,8 @@
 use lazy_static::lazy_static;
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::fmt;
 
 type Offsets = (i32, i32);
@@ -171,6 +173,7 @@ fn update(
 }
 
 impl Tetris {
+    /// Create a new tetris game state object.
     pub fn new() -> Self {
         let mut grid: Grid = [[0; 10]; 20];
         update(&mut grid, Piece::O, 0, 0, 4, true);
@@ -183,7 +186,132 @@ impl Tetris {
         }
     }
 
-    pub fn tick() {}
+    /// Fetch all positions of the current falling piece.
+    fn falling_piece_positions(&self) -> Vec<(i32, i32)> {
+        // TODO: a length-4 slice is fine, and we avoid allocation.
+        let mut results = Vec::new();
+        let rotation_offsets: &[Offsets4; 4] = ROTATION_OFFSETS.get(&self.piece).unwrap();
+        for (off_row, off_col) in rotation_offsets[self.rotation as usize].iter() {
+            results.push((self.anchor_row + off_row, self.anchor_col + off_col));
+        }
+        results
+    }
+
+    /// Checks if a new piece at the given row, col, and rotation
+    /// overlaps with any existing cells.
+    fn fits(grid: &Grid, piece: Piece, row: i32, col: i32, rotation: i32) -> bool {
+        let rotation_offsets: &[Offsets4; 4] = ROTATION_OFFSETS.get(&piece).unwrap();
+        for (off_row, off_col) in rotation_offsets[rotation as usize].iter() {
+            let this_row = off_row + row;
+            let this_col = off_col + col;
+            if grid[this_row as usize][this_col as usize] == 1 {
+                return false;
+            }
+        }
+        true
+    }
+
+    /// Tests whether current falling piece can drop one more unit or
+    /// not.
+    fn can_drop(&self) -> bool {
+        let positions = self.falling_piece_positions();
+        for (row, col) in positions.iter() {
+            let next_row = row + 1;
+            if next_row == NROWS as i32 {
+                return false;
+            }
+            // if the cell 1 unit down is not part of the piece
+            // itself, and that the cell is filled, then we cannot
+            // drop further.
+            if !positions.contains(&(next_row, *col))
+                && self.grid[next_row as usize][*col as usize] == 1
+            {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    /// Shift everything above row down by 1.
+    fn shift_down(&mut self, row: i32) {
+        for r in (1..row as usize).rev() {
+            for c in 0..NCOLS {
+                self.grid[r][c] = self.grid[r - 1][c];
+            }
+        }
+        for c in 0..NCOLS {
+            self.grid[0][c] = 0;
+        }
+    }
+
+    /// Simulate "gravity" for 1 unit of time. Returns true if the game can still continue
+    /// otherwise returns false.
+    pub fn tick(&mut self) -> bool {
+        let should_continue: bool;
+        // 1. if we can drop, then just drop
+        // 2. if we cannot drop, then check if there are complete rows
+        // 3. check if there is enough space for new piece
+        if self.can_drop() {
+            update(
+                &mut self.grid,
+                self.piece,
+                self.rotation,
+                self.anchor_row,
+                self.anchor_col,
+                false,
+            );
+            self.anchor_row += 1;
+            update(
+                &mut self.grid,
+                self.piece,
+                self.rotation,
+                self.anchor_row,
+                self.anchor_col,
+                true,
+            );
+            should_continue = true;
+        } else {
+            // 2. check for complete rows
+            let mut min_row: i32 = NROWS as i32;
+            let mut max_row: i32 = 0;
+
+            for (row, _) in self.falling_piece_positions().iter() {
+                min_row = min(min_row, *row);
+                max_row = max(max_row, *row);
+            }
+
+            // shift things down by 1 if there are complete rows.
+            for row in min_row..=max_row {
+                if self.grid[row as usize].iter().sum::<i32>() == NCOLS as i32 {
+                    self.shift_down(row);
+                }
+            }
+
+            let new_piece: Piece = ((self.piece as i32 + 1) % 7).try_into().unwrap();
+            if Self::fits(&self.grid, new_piece, 0, 4, 0) {
+                self.piece = new_piece;
+                self.rotation = 0;
+                self.anchor_row = 0;
+                self.anchor_col = 4;
+                should_continue = true;
+
+                update(
+                    &mut self.grid,
+                    self.piece,
+                    self.rotation,
+                    self.anchor_row,
+                    self.anchor_col,
+                    true,
+                );
+            } else {
+                should_continue = false;
+            }
+        }
+
+        should_continue
+    }
+
     pub fn event() {}
 }
 
@@ -217,6 +345,71 @@ Piece: O, Rotation: 0, ARow: 0, ACol: 4
 0000000000
 0000000000
 0000000000
+"#;
+        assert_eq!(repr, expected);
+    }
+
+    #[test]
+    fn test_tick() {
+        let mut t = Tetris::new();
+        t.tick();
+        let repr = format!("{:?}", t);
+        println!("{}", repr);
+        let expected = r#"========TETRIS========
+Piece: O, Rotation: 0, ARow: 1, ACol: 4
+0000000000
+0000110000
+0000110000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+"#;
+        assert_eq!(repr, expected);
+    }
+
+    #[test]
+    fn test_tick_until_bottom() {
+        let mut t = Tetris::new();
+        for _ in 0..19 {
+            t.tick();
+        }
+        let repr = format!("{:?}", t);
+        let expected = r#"========TETRIS========
+Piece: L, Rotation: 0, ARow: 0, ACol: 4
+0000100000
+0000100000
+0000110000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000110000
+0000110000
 "#;
         assert_eq!(repr, expected);
     }
